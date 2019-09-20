4bda596e6110287e8722e91b134ddad3d21767ab
Lucas <you@example.com>
2019-09-20 20:12:47 -0300

    commitando portfolio

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(window).load(function() {
+   $('.preloader').fadeOut('slow');
+
+});
+
+$(document).ready(function(){
+        $('.materialboxed').materialbox();
+        }); 
